ae6f91ad4d5b5f836b39ee4bbcaeb4763c3306a6
Daniel Adler <weston.adler@gmail.com>
2015-12-03 11:56:10 -0800

    slight adjustments in login form and display of admin options link; commit before adjust routes and controllers to accomodate admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,26 @@
 Rails.application.routes.draw do
 
-  get  "/sign-up"               , to: "users#sign_up"
-  post "/users"                 , to: "users#create"
+  get  "/sign-up"                  , to: "users#sign_up"
+  post "/users"                    , to: "users#create"
 
-  post "/sign-in"               , to: "sessions#sign_in" , as: :sign_in
-  get  "/sign-out"              , to: "sessions#sign_out", as: :sign_out
+  post "/sign-in"                  , to: "sessions#sign_in" , as: :sign_in
+  get  "/sign-out"                 , to: "sessions#sign_out", as: :sign_out
 
-  get    "/"                       , to: "products#index"   , as: :home
+  get  "/"                          , to: "products#index"   , as: :home
+  get  "/admin/products"            , to: "products#admin"   , as: :products
+  get  "/admin/products/new"        , to: "products#new"     , as: :new_product
+  post "/admin/products"            , to: "products#add"     , as: :add_product
+  get  "/admin/products/:product_id", to: "products#admin_product"  , as: :admin_product
+
   get    "/products/:product_id"   , to: "products#show"    , as: :show_product
+  patch  "/products/:product_id"   , to: "products#edit"    , as: :edit_product
+  delete "/products/:product_id"   , to: "products#delete"  , as: :delete_product
 
   post   "/products/:product_id"   , to: "product_options#add"     , as: :product_options
   patch  "/products/:product_id"   , to: "product_options#edit"    , as: :edit_option
   delete "/products/:product_id"   , to: "product_options#delete"  , as: :delete_option
 
-
-  get "/admin/products"            , to: "products#admin"   , as: :products
-  get "/admin/products/new"        , to: "products#new"     , as: :new_product
-  get "/admin/products/:product_id", to: "products#admin_product", as: :admin_product
-  patch  "/products/:product_id"   , to: "products#edit"    , as: :edit_product
-  delete "/products/:product_id"   , to: "products#delete"  , as: :delete_product
-
-  post "/admin/products"         , to: "products#add"   , as: :add_product
-
+  get "/admin/orders"              , to: "orders#index"     , as: :orders
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
